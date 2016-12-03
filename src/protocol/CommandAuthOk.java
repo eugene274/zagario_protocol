e2e66ccb2018b75c10c1187df10bd0ec3a3820bd
Kashirin Eugene <kashirin.e@list.ru>
2016-12-03 14:38:25 +0300

    CommandAuthOk consumes playerID, version change to 1.1

--- src/protocol/CommandAuthOk.java
+++ src/protocol/CommandAuthOk.java
@@ -9,7 +9,15 @@ public final class CommandAuthOk extends Command {
   @NotNull
   public static final String NAME = "auth_ok";
 
+  public int playerID;
+
+  @Deprecated
   public CommandAuthOk() {
     super(NAME);
+  }
+
+  public CommandAuthOk(@NotNull String name, int playerID) {
+    super(name);
+    this.playerID = playerID;
   }
 }
